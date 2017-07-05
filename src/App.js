ebf39aa06b6ccbda675a2b3a43007b64d7bce0c0
Bikramjit Ganguly <bikramjit@aylanetworks.com>
2017-07-04 18:39:18 -0700

    Udacity React Nanodegree Module 2 lesson 5 all chapters completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
  * Created by bganguly on 6/28/17.
  */
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state= {
-    screen: 'list',
     contacts : []
   }
 
@@ -25,24 +25,29 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
-  navigateToCreate = () => {
-    this.setState ({
-      screen:'create'
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
     })
   }
   render() {
     return (
       <div className="app">
-        {this.state.screen === 'list' && (
+        <Route exact path="/" render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
-            onNavigate={this.navigateToCreate}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact/>
-        )}
+        )}/>
+        <Route path="/create" render = {( { history }) => (
+          <CreateContact
+          onCreateContact={(contact)=>{
+            this.createContact(contact)
+            history.push("/")
+          }}/>
+        )}/>
       </div>
     )
   }
